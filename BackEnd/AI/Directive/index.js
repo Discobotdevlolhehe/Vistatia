6502c84f5972d9ae85a32a302d6d5e488bd34ab7
Unknownleaf <94049800+Discobotdevlolhehe@users.noreply.github.com>
2025-06-21 16:00:51 +0530

    Update index.js

--- BackEnd/AI/Directive/index.js
+++ BackEnd/AI/Directive/index.js
@@ -2,7 +2,7 @@ const ax = require("axios");
 const fz = require("fs");
 const pp = require("path");
 
-const KEY = ""; // API sadness key
+const KEY = "placeholder"; // API sadness key
 const BLOB = pp.join(__dirname, "..", "shared_memory.json"); // mental vomit storage
 
 // psychic memory recall
